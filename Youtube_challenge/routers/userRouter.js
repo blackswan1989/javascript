69d6965dc5315a6c262a2ef95b58387fb8aed7c7
blackswan1989 <67410919+blackswan1989@users.noreply.github.com>
2020-09-18 23:15:12 +0900

    day5 upload

---
+++ Youtube_challenge/routers/userRouter.js
@@ -0,0 +1,18 @@
+import express from "express";
+import routes from "../routes";
+import {
+  userDetail,
+  users,
+  editProfile,
+  changePassword,
+} from "../controllers/userController";
+
+const userRouter = express.Router();
+
+userRouter.get(routes.users, users);
+// userRouter.get("/", users); "http://localhost:4000/users" 주소를 만드려면 이렇게.
+userRouter.get(routes.userDetail, userDetail);
+userRouter.get(routes.editProfile, editProfile);
+userRouter.get(routes.changePassword, changePassword);
+
+export default userRouter;
