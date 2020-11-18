81db14580aaeb2e7b581cbce92aeb304189751ad
blackswan1989 <67410919+blackswan1989@users.noreply.github.com>
2020-11-18 21:10:53 +0900

    JS : global variable

--- JS-Basic/DoIt-js-basic-book-master/05/js/global.js
+++ JS-Basic/DoIt-js-basic-book-master/05/js/global.js
@@ -1,7 +1,12 @@
+// NOTE 변수의 적용 범위
+
+//* Global Variable : 스크립트 소스 전체에서 사용할 수 있는 전역 변수
+
 var myVar = 100;
 test();
 document.write("myVar is " + myVar);
 
-function test() { 			
-	myVar = 50;
+function test() {
+  myVar = 50;
+  // 지역변수 myVar 앞에 var 예약어를 붙이지 않아 전역변수가 되어 50이 출력된다.
 }
