850ac21c7824a3ab351d9654cec9bcf6784b39ab
blackswan1989 <67410919+blackswan1989@users.noreply.github.com>
2020-11-21 21:02:36 +0900

    JS: practice 이벤트 처리기를 사용해 함수 실행하기.

--- JS-Basic/DoIt-js-basic-book-master/05/js/diy-event.js
+++ JS-Basic/DoIt-js-basic-book-master/05/js/diy-event.js
@@ -1,9 +1,21 @@
-		function showDetail() {  // '상세 설명 보기'를 클릭했을 때 상세 설명을 보여주는 함수
-			document.querySelector('#desc').style.display = "block";	// 상세 설명 부분을 화면에 표시
-			document.querySelector('#open').style.display = "none";   // '상세 설명 보기' 단추를 화면에서 감춤
-		}
-		
-		function hideDetail() {  // '상세 설명 닫기'를 클릭했을 때 상세 설명을 감추는 함수
-			document.querySelector('#desc').style.display = "none";	   // 상세 설명 부분을 화면에서 감춤
-			document.querySelector('#open').style.display = "block";	 // '상세 설명 보기' 단추를 화면에 표시
-		}
+const openBtn = document.querySelector("#open");
+openBtn.addEventListener("click", showDetail);
+
+// '상세 설명 보기'를 클릭했을 때 상세 설명을 보여주는 함수
+function showDetail() {
+  // 상세 설명 부분을 화면에 표시
+  document.querySelector("#desc").style.display = "block";
+  // '상세 설명 보기' 단추를 화면에서 감춤
+  document.querySelector("#open").style.display = "none";
+}
+
+const closeBtn = document.querySelector("#close");
+closeBtn.addEventListener("click", hideDetail);
+
+// '상세 설명 닫기'를 클릭했을 때 상세 설명을 감추는 함수
+function hideDetail() {
+  // 상세 설명 부분을 화면에서 감춤
+  document.querySelector("#desc").style.display = "none";
+  // '상세 설명 보기' 단추를 화면에 표시
+  document.querySelector("#open").style.display = "block";
+}
