cec04c33b9848373b816d16d378dab6b1d275db4
blackswan1989 <67410919+blackswan1989@users.noreply.github.com>
2020-11-24 22:47:06 +0900

    JS: practice - 리터럴표기법으로 robot 객체만들기

---
+++ JS-Basic/DoIt-js-basic-book-master/06_Object/js/object.js
@@ -0,0 +1,22 @@
+const toyRobot = {
+  // toyRobot 객체를 선언한 후 속성과 함수 정의
+  productId: "123-12",
+  name: "Robot",
+  price: "25,000",
+  madeIn: "Korea",
+  quantity: 10,
+  showStock: function () {
+    // showStock() 함수 정의
+    document.querySelector(
+      "#displayQuantity"
+    ).innerHTML = `${this.name} 제품은 ${this.quantity}개 남아있습니다.`;
+  },
+  showPrice: function () {
+    document.querySelector(
+      "#displayPrice"
+    ).innerHTML = `ID ${this.productId}제품의 가격은 ${this.price}이며 ${this.madeIn} 에서 만들어졌습니다.`;
+  },
+};
+
+toyRobot.showStock(); // toyRobot 객체의 showStock() 함수 실행
+toyRobot.showPrice();
