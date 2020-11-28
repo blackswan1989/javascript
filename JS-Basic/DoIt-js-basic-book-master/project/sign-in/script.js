15ef59af402d9884df048b1b02247faa789c0444
blackswan1989 <67410919+blackswan1989@users.noreply.github.com>
2020-11-29 00:21:34 +0900

    project : Sign in gender check & agree check

--- JS-Basic/DoIt-js-basic-book-master/project/sign-in/script.js
+++ JS-Basic/DoIt-js-basic-book-master/project/sign-in/script.js
@@ -1,6 +1,7 @@
 "use strict";
 
 const submit = document.querySelector(".submit__input");
+const agreeCheckBox = document.querySelector("#agree");
 
 const form = function () {
   const userName = document.querySelector("#userName").value;
@@ -9,32 +10,24 @@ const form = function () {
   const userPasswordConfirm = document.querySelector("#userPassword2").value;
   const userBirth = document.querySelector("#userBirth").value;
 
-  console.log(userName);
-  console.log(userId);
-  console.log(userPassword);
-  console.log(userPasswordConfirm);
-  console.log(userBirth);
-
   const checkSpecial = /[`~!@#$%^&*|\\\'\";:\/?]/gi;
 
   const nameCheck = function () {
+    const messageName = document.querySelector(".messageName");
     if (userName == null || userName == "") {
-      document.querySelector(".messageName").innerHTML = "이름을 입력해주세요.";
+      messageName.innerHTML = "이름을 입력해주세요.";
       document.querySelector("#userName").focus();
     } else if (userName.search(/\s/) != -1) {
-      document.querySelector(".messageName").innerHTML =
-        "이름은 빈칸을 포함할 수 없습니다.";
+      messageName.innerHTML = "이름은 빈칸을 포함할 수 없습니다.";
       document.querySelector("#userName").focus();
     } else if (userName.length < 3 || userName.length > 20) {
-      document.querySelector(".messageName").innerHTML =
-        "이름은 3글자 이상 20글자 이하로 작성해주세요.";
+      messageName.innerHTML = "이름은 3글자 이상 20글자 이하로 작성해주세요.";
       document.querySelector("#userName").focus();
     } else if (checkSpecial.test(userName)) {
-      document.querySelector(".messageName").innerHTML =
-        "이름에 특수문자는 사용할 수 없습니다.";
+      messageName.innerHTML = "이름에 특수문자는 사용할 수 없습니다.";
       document.querySelector("#userName").focus();
     } else {
-      document.querySelector(".messageName").innerHTML = "";
+      messageName.innerHTML = "";
     }
   };
 
@@ -93,13 +86,7 @@ const form = function () {
       document.querySelector(".messagePassword2").innerHTML =
         "비밀번호는 6글자 이상 20글자 이하로 작성해주세요.";
       document.querySelector("#userPassword2").focus();
-    } else {
-      document.querySelector(".messagePassword2").innerHTML = "";
-    }
-  };
-
-  function passwordMatchCheck() {
-    if (userPassword !== userPasswordConfirm) {
+    } else if (userPassword !== userPasswordConfirm) {
       document.querySelector(".messagePassword1").innerHTML =
         "비밀번호가 서로 일치하지 않습니다.";
       document.querySelector("#userPassword").value = "";
@@ -107,35 +94,80 @@ const form = function () {
         "비밀번호가 서로 일치하지 않습니다.";
       document.querySelector("#userPassword2").value = "";
       document.querySelector("#userPassword").focus();
+    } else {
+      document.querySelector(".messagePassword2").innerHTML = "";
     }
-  }
+  };
 
-  function agreeCheck() {
-    var chkbox = document.querySelector("#agree");
+  const birthCheck = function () {
+    if (userBirth == "") {
+      document.querySelector(".messageBirth").innerHTML =
+        "달력에서 생년월일을 선택해주세요.";
+      return true;
+    } else {
+      document.querySelector(".messageBirth").innerHTML = "";
+    }
+  };
 
-    if (chkbox.checked) {
+  const agreeCheck = function () {
+    if (agreeCheckBox.checked) {
       document.querySelector(".messageCheckBox").innerHTML =
         "✅ 모든 약관에 동의하였습니다.";
+      return true;
     } else {
       document.querySelector(".messageCheckBox").innerHTML =
         "⛔️ 전체 약관에 동의해주세요.";
+      return false;
     }
-  }
+  };
 
   function init() {
     nameCheck();
     idCheck();
     passwordCheck();
     passwordConfirmCheck();
-    passwordMatchCheck();
+    birthCheck();
     agreeCheck();
   }
   init();
 };
 
+agreeCheckBox.addEventListener("click", () => {
+  if (agreeCheckBox.checked) {
+    document.querySelector(".messageCheckBox").innerHTML =
+      "✅ 모든 약관에 동의하였습니다.";
+  } else {
+    document.querySelector(".messageCheckBox").innerHTML =
+      "⛔️ 전체 약관에 동의해주세요.";
+    return false;
+  }
+});
+
+const genderCheck = function () {
+  const genderMan = document.querySelector("#userGenderMan");
+  const genderWoman = document.querySelector("#userGenderWoman");
+
+  genderMan.addEventListener("click", () => {
+    if (genderMan.checked) {
+      document.querySelector(".messageGender").innerHTML =
+        "🚹 '남자'를 선택하였습니다.";
+    }
+  });
+
+  genderWoman.addEventListener("click", () => {
+    if (genderWoman.checked) {
+      document.querySelector(".messageGender").innerHTML =
+        "🚺 '여자'를 선택하였습니다.";
+    }
+  });
+};
+genderCheck();
+
 submit.addEventListener("click", form);
 
 //NOTE 참조한 사이트
-//https://grace-go.tistory.com/66
+//유효성 검사 | https://grace-go.tistory.com/66
 //https://backstreet-programmer.tistory.com/53
 //https://bookk.tistory.com/65 이름에 한글,영문,숫자만 허용되도록
+//HTML5 폼 검증 | https://jeonghwan-kim.github.io/dev/2020/06/08/html5-form-validation.html
+// 자동 submit 방지 | https://fruitdev.tistory.com/151
