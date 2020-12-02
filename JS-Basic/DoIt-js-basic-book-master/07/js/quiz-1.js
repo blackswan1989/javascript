4949189b9922dd3884895d859cadba2290ee70e4
blackswan <67410919+blackswan1989@users.noreply.github.com>
2020-12-02 12:19:21 +0900

    JS: quiz 1-2

--- JS-Basic/DoIt-js-basic-book-master/07/js/quiz-1.js
+++ JS-Basic/DoIt-js-basic-book-master/07/js/quiz-1.js
@@ -1,11 +1,21 @@
-		var numbers = [2, 4, 6, 8, 10];
-		showArray(numbers);
+var numbers = [2, 4, 6, 8, 10];
+showArray(numbers);
 
-		function showArray(arr) {
-			var str = "<table><tr>";
-			for (var i=0; i<arr.length; i++) {
-				str += "<td>" + arr[i] + "</td>";
-			}
-			str += "</tr></table>";
-			document.write(str);
-		}
+let sum = 0;
+for (let i = 0; i < numbers.length; i++) {
+  sum = sum + numbers[i];
+}
+
+console.log(numbers[0] + numbers[1]);
+
+numbers.push(sum);
+showArray(numbers);
+
+function showArray(arr) {
+  var str = "<table><tr>";
+  for (var i = 0; i < arr.length; i++) {
+    str += "<td>" + arr[i] + "</td>";
+  }
+  str += "</tr></table>";
+  document.write(str);
+}
