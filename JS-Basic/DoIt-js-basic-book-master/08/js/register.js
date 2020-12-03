7f6ffa8a7602ea20bf889a89286db2faa2ce701c
blackswan <67410919+blackswan1989@users.noreply.github.com>
2020-12-03 11:27:44 +0900

    JS: register practice

--- JS-Basic/DoIt-js-basic-book-master/08/js/register.js
+++ JS-Basic/DoIt-js-basic-book-master/08/js/register.js
@@ -4,12 +4,12 @@
 
 function newRegister() {
   // #userName > p tag > text part
+
   const newP = document.createElement("p"); // 새로운 p element 생성
   const userName = document.querySelector("#userName"); // input의 텍스트 필드 내용 가져오기
   const newText = document.createTextNode(userName.value); // 가져온 userName 필드의 value 값으로 새로운 택스트 노드 생성
   newP.appendChild(newText); // newText노드를 appendChild()함수를 사용해 부모 element노드(newP)에 서로 연결하기
   console.log(newText);
-
   // p tag > span btn "X" part
   const delBtn = document.createElement("span"); // 새로운 span btn element 만들기
   const delText = document.createTextNode("X"); // 새로운 text node 만들기
@@ -24,8 +24,18 @@ function newRegister() {
 
   // "X" btn delete part
   // span X 버튼이 클릭 이벤트를 발동시켰을때 삭제되어야 할 대상은 p tag이다.
-  // 하지만 노드 삭제는 부모 노드에서만 가능하기 때문에 p의 부모인 #nameList 노드에서 처리해야 한다.
+  // 하지만 노드 삭제는 부모 노드에서만 가능하기 때문에 x버튼의 부모인 p의 부모, 그리고 p버튼의 부모인 #nameList 노드에서 처리해야 한다.
   const removeBtn = document.querySelectorAll(".del");
+  for (let i = 0; i < removeBtn.length; i++) {
+    // removeBtn에 있는 element 전체를 반복
+    removeBtn[i].addEventListener("click", function () {
+      // i번째 버튼을 눌렀을 때 실행할 함수 선언
+      if (this.parentNode.parentNode) {
+        // 현재 노드(this : X버튼)의 부모 노드의 부모 노드가 있을 경우 실행
+        this.parentNode.parentNode.removeChild(this.parentNode); // 현재 노드(this : X버튼) 부모(p)를 삭제하기 위해 p의 부모(#nameList)를 찾고 그 자식인 p노드를 removeChild()로 삭제
+      }
+    });
+  }
   //p.285
 }
 
